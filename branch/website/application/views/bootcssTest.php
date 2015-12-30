ebaaa0ecdecba6e33d612540ba572aa919fd7581
linzequan <lowkey361@gmail.com>
2015-12-30 10:22:58 +0800

    移动到branch分支

---
+++ branch/website/application/views/bootcssTest.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>bootcss test</title>
+    <!-- 新 Bootstrap 核心 CSS 文件 -->
+    <link rel="stylesheet" href="<?php echo base_url('/public/css/bootstrap.min.css'); ?>">
+    <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+    <script src="<?php echo base_url('/public/js/jquery.min.js'); ?>"></script>
+    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+    <script src="<?php echo base_url('/public/js/bootstrap.min.js'); ?>"></script>
+</head>
+<body>
+    <table class="table">
+        <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Sex</th>
+        </tr>
+        <tr>
+            <td>aa</td>
+            <td>aa</td>
+            <td>aa</td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ branch/website/application/views/public/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>登陆</title>
+        <?php
+            $base_url = base_url('/application/views/assets').'/';
+        ?>
+
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <!-- basic styles -->
+
+        <link rel="stylesheet" href="<?php echo $base_url; ?>css/bootstrap.min.css" />
+        <link rel="stylesheet" href="<?php echo $base_url; ?>css/font-awesome.min.css" />
+
+        <!-- bootstrap & fontawesome -->
+        <link rel="stylesheet" href="<?php echo $base_url; ?>css/font-awesome.min.css" />
+
+        <!--[if IE 7]>
+          <link rel="stylesheet" href="<?php echo $base_url; ?>css/font-awesome-ie7.min.css" />
+        <![endif]-->
+
+        <!-- page specific plugin styles -->
+
+        <!-- fonts -->
+
+        <link rel="stylesheet" href="<?php echo $base_url; ?>css/fonts.css" />
+
+        <!-- page specific plugin styles -->
+        <link rel="stylesheet" href="<?php echo $base_url; ?>css/jquery-ui.custom.min.css" />
+
+        <!-- text fonts -->
+        <link rel="stylesheet" href="<?php echo $base_url; ?>font/fonts.googleapis.com.css" />
+
+        <!-- ace styles -->
+
+        <link rel="stylesheet" href="<?php echo $base_url; ?>css/ace.min.css" />
+        <link rel="stylesheet" href="<?php echo $base_url; ?>css/ace-rtl.min.css" />
+        <link rel="stylesheet" href="<?php echo $base_url; ?>css/ace-skins.min.css" />
+        <link rel="stylesheet" href="<?php echo $base_url; ?>css/jquery.gritter.css" />
+
+
+        <!--[if lte IE 8]>
+          <link rel="stylesheet" href="<?php echo $base_url; ?>css/ace-ie.min.css" />
+        <![endif]-->
+
+        <!-- inline styles related to this page -->
+
+        <!-- ace settings handler -->
+
+        <script src="<?php echo $base_url; ?>js/ace-extra.min.js"></script>
+
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+
+        <!--[if lt IE 9]>
+        <script src="<?php echo $base_url; ?>js/html5shiv.js"></script>
+        <script src="<?php echo $base_url; ?>js/respond.min.js"></script>
+        <![endif]-->
+    </head>
